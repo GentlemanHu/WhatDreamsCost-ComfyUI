3725151cd212d96f297a2b690ead2e0e6681e026
WhatDreamsCost <whatdreamscost@gmail.com>
2026-03-25 15:24:45 -0500

    Added new node "Speech Length Calculator"

--- __init__.py
+++ __init__.py
@@ -1,19 +1,22 @@
 from .ltx_keyframer import LTXKeyframer
 from .multi_image_loader import MultiImageLoader
 from .ltx_sequencer import LTXSequencer
+from .speech_length_calculator import SpeechLengthCalculator
 
 # Register the node classes
 NODE_CLASS_MAPPINGS = {
     "LTXKeyframer": LTXKeyframer,
     "MultiImageLoader": MultiImageLoader,
-    "LTXSequencer": LTXSequencer
+    "LTXSequencer": LTXSequencer,
+    "SpeechLengthCalculator": SpeechLengthCalculator
 }
 
 # Provide clean display names for the ComfyUI interface
 NODE_DISPLAY_NAME_MAPPINGS = {
     "LTXKeyframer": "LTX Keyframer",
     "MultiImageLoader": "Multi Image Loader",
-    "LTXSequencer": "LTX Sequencer"
+    "LTXSequencer": "LTX Sequencer",
+    "SpeechLengthCalculator": "Speech Length Calculator"
 }
 
 WEB_DIRECTORY = "./js"
